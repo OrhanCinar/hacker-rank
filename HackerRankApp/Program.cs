a27e1af0893adf3ce841991331ffbcbaec976b0e
Orhan CINAR <orhancinar35@gmail.com>
2019-02-26 02:38:43 +0100

    init

---
+++ HackerRankApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using HackerRankApp.Warmup;
+using System;
+
+namespace HackerRankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var C = new SockMerchant();
+            var socks = new[] { 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 };
+            var t = C.PairSocks(10, socks);
+            Console.WriteLine(t);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HackerRankApp/WarmpUp/SockMerchant.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankApp.Warmup
+{
+    public class SockMerchant
+    {
+        public int PairSocks(int count, int[] socks)
+        {
+            var dic = new Dictionary<int, int>();
+
+            foreach (var sock in socks)
+            {
+                if (dic.ContainsKey(sock))
+                {
+                    var value = 0;
+                    var item = dic.TryGetValue(sock, out value);
+                    dic[sock] = ++value;
+                }
+                else
+                {
+                    dic.Add(sock, 1);
+                }
+            }
+
+            var pairCount = 0;
+
+
+            foreach (var item in dic)
+            {
+                var totalPairs = item.Value % 2;
+
+                if (totalPairs == 0)
+                {
+                    pairCount += item.Value / 2;
+                }   
+                else
+                {
+                    var evenPair = item.Value / 2;
+
+                    pairCount += evenPair;
+                }
+            }
+
+            return pairCount;
+        }
+    }
+}
